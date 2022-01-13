8c05ad0ed1acf37edfc5435fa85a67aa76cbf224
Shadae96 <84427990+Shadae96@users.noreply.github.com>
2022-01-12 22:02:40 -0500

    pushing completed code

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,15 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const util= require ('util');
 // const prompt = inquirer;
 
 // const prompt = (inquirer)
 
 // TODO: Create an array of questions for user input
 
-const questions = 
-inquirer
-.prompt([
+const questions = [
 { 
     type:"input",
     name:"title",
@@ -72,18 +71,20 @@ inquirer
     message:"What is your email address?",
     },
 
-])
+];
 
 
 // // TODO: Create a function to write README file
 
 function writeToFile(fileName, data) {
-fs.writeFile(generateMarkdown, data, (err) => {
+fs.writeFile(fileName, data, (err) => {
         err
           ? console.log(err)
           : console.log("GENERATED-README.md was successfully created");
       });
     }
+
+const writeFileAsync = util.promisify(writeToFile);
     
 
 
@@ -108,7 +109,7 @@ async function init() {
         console.log(markdown);
     
         // Write markdown to file
-        await writeToFile('GENERATED-README.md', markdown);
+        await writeFileAsync('GENERATED-README.md', markdown);
 
     } catch (error) {
         console.log(error);
@@ -116,4 +117,4 @@ async function init() {
 };
 
 // // Function call to initialize app
-// init();
+init();
